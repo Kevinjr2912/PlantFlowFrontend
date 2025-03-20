5a8ff870c37201d151a5ec39189c8716bde86c09
Kevin <moralesperezkevin7@gmail.com>
2025-03-20 02:39:41 -0600

    Componente listo para poder hacer ciertas acciones con las parcelas (HTML,CSS)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { ParcelModule } from './presentation/parcel/parcel.module';
 import { CropModule } from './presentation/crop/crop.module';
 import { AsideComponent } from "./presentation/shared/aside/aside.component";
 import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './presentation/shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/presentation/parcel/parcel.module.ts
+++ src/app/presentation/parcel/parcel.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParcelPageComponent } from './parcel-page/parcel-page.component';
-
-
+import { ActionsParcelsComponent } from './actions-parcels/actions-parcels.component';
 
 @NgModule({
   declarations: [
-    ParcelPageComponent
+    ParcelPageComponent,
+    ActionsParcelsComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ParcelPageComponent
   ]
 })
 export class ParcelModule { }

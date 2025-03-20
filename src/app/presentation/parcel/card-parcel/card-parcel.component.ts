05e60739f725b0fc94eae5b86afbfefa915c501c
Kevin <moralesperezkevin7@gmail.com>
2025-03-20 03:51:13 -0600

    Card de parcela lista

---
+++ src/app/presentation/parcel/card-parcel/card-parcel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-parcel',
+  templateUrl: './card-parcel.component.html',
+  styleUrl: './card-parcel.component.css'
+})
+export class CardParcelComponent {
+
+}
--- src/app/presentation/parcel/parcel.module.ts
+++ src/app/presentation/parcel/parcel.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParcelPageComponent } from './parcel-page/parcel-page.component';
 import { ActionsParcelsComponent } from './actions-parcels/actions-parcels.component';
+import { CardParcelComponent } from './card-parcel/card-parcel.component';
 
 @NgModule({
   declarations: [
     ParcelPageComponent,
-    ActionsParcelsComponent
+    ActionsParcelsComponent,
+    CardParcelComponent
   ],
   imports: [
     CommonModule

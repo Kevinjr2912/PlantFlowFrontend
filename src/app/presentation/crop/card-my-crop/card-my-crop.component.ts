451c1eb7c8d1998267cdca42f01fb0dccc9a9945
Andresito126 <creed126ax@gmail.com>
2025-03-20 09:03:16 -0600

    Add: givint the html structure to crops

---
+++ src/app/presentation/crop/card-my-crop/card-my-crop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-my-crop',
+  templateUrl: './card-my-crop.component.html',
+  styleUrl: './card-my-crop.component.css'
+})
+export class CardMyCropComponent {
+
+}
--- src/app/presentation/crop/crop.module.ts
+++ src/app/presentation/crop/crop.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CropPageComponent } from './crop-page/crop-page.component';
+import { CardMyCropComponent } from './card-my-crop/card-my-crop.component';
+import { CardStatisticsComponent } from './card-statistics/card-statistics.component';
+import { CardParametersComponent } from './card-parameters/card-parameters.component';
 
 
 
@@ -8,6 +11,9 @@ import { CropPageComponent } from './crop-page/crop-page.component';
 @NgModule({
   declarations: [
     CropPageComponent,
+    CardMyCropComponent,
+    CardStatisticsComponent,
+    CardParametersComponent,
   
   ],
   imports: [
